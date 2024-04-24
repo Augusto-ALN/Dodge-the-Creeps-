c53eab4f147762908f96a639cfc29033eccc2c8c
Augusto Alves <augustoaln20@gmail.com>
2024-04-24 03:05:38 -0300

    Created HUD elements and synchronized with main scene

--- main.gd
+++ main.gd
@@ -5,19 +5,23 @@ var score
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
-	new_game()
+	pass
 	
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
