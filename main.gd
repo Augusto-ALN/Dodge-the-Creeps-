a5b1825edfca594ab30693d12421c958c3674cae
Augusto Alves <augustoaln20@gmail.com>
2024-04-24 03:09:52 -0300

    All mobs get deleted on GameOver Screen

--- main.gd
+++ main.gd
@@ -11,6 +11,7 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	get_tree().call_group("mobs", "queue_free")
 	
 func new_game():
 	score = 0
@@ -18,6 +19,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+
 
 func _on_score_timer_timeout():
 	score += 1
